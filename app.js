3f1d668e54e497e578ac97ba2dbf3f2bde86ead0
naeemalomari <naeemalomari96@gmail.com>
2021-05-27 16:35:53 +0300

    problem3

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ Write a function called sum() that takes in two numbers as arguments and then re
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
-function sum(a, b) {
+// function sum(a, b) {
 // Write your code here
-}
+// }
 // Here is the test for sum(); uncomment it to run it
 // testSum(4, 7);
 
@@ -24,9 +24,9 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) {
+// function multiply(a, b) {
   //eslint-disable-line
-}
+// }
 
 // Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
@@ -35,7 +35,11 @@ function multiply(a, b) {
 
 /////////////////////////////////////
 /* Problem 3
-Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
+Write a function called sumAndMultiply() that takes in three numbers as separate arguments 
+and returns an array where the first element is the sum of those three numbers,
+ the second element is the product of those three numbers,
+   and the third and fourth elements are strings that EXACTLY follow this example 
+   and use the values that were input into the function:
 
 Third element: "4 and 7 and 5 sum to 16."
 Fourth element: "The product of 4 and 7 and 5 is 140."
@@ -46,11 +50,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) {
+let sum= a +b +c ;
+let multiply= a*b*c;
+let stringSum= "4 and 7 and 5 sum to 16.";
+let stringMultiply= "The product of 4 and 7 and 5 is 140.";
+return[sum,multiply,stringSum,stringMultiply];
   //eslint-disable-line
 }
-
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
