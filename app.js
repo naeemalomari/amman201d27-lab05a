eeaa89c92950ab228079bc653cc80a2803faa49a
naeemalomari <naeemalomari96@gmail.com>
2021-05-27 16:18:31 +0300

    problem2

--- app.js
+++ app.js
@@ -17,19 +17,26 @@ function sum(a, b) {
 
 /////////////////////////////////////
 /* Problem 2
-Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called multiply() that takes in two numbers as arguments
+ and returns an array where the first element is the product of those numbers
+ , and the second element is a string that EXACTLY follows this example 
+ and uses the values that were input into the function:
 
 "The product of 5 and 9 is 45."
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
+Test this function by hand in the console to get it working,
+ and when you think it is finished, uncomment the call for the testMultiply() function
+  and see if the test passes.*/
 
 // Write your code here
 function multiply(a, b) {
+let o= a*b ;
+let productOfMultiplay = "The product of 5 and 9 is 45.";
+return [o,productOfMultiplay];
   //eslint-disable-line
 }
-
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
