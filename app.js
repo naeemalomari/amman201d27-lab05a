c4b6f9febd1270ce39ee88de270c8ad07692d70c
naeemalomari <naeemalomari96@gmail.com>
2021-05-29 16:40:55 +0300

    problem3

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ function multiply(a, b) {
   testMultiply(5,9);
 // Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
-
+// console.log ();
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
